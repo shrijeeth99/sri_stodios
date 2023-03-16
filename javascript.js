153a9e8578a89b98c49e85c6c0acb943b991401a
shrijeeth99 <125179283+shrijeeth99@users.noreply.github.com>
2023-03-16 23:31:56 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ function scrollFunction()
 {
   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) 
   {
-    document.getElementById("navbar").style.background = "black";
+    document.getElementById("navbar").style.background = "rgb(33 57 65)";
   } else {
     document.getElementById("navbar").style.background = "none";
   }
